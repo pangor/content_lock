37eccbfc3dffb7c582b9330477593d79a64470c6
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2011-06-30 08:55:15 -0400

    Fix issue #1203812 by ohnobinki: Use Drupal.behaviors to initialize content_lock's javascript.

--- js/content_lock_init.js
+++ js/content_lock_init.js
@@ -3,11 +3,12 @@
  *   Initialize onUnload scripts.
  */
 
-Drupal.behaviors.content_lock = function() {
+Drupal.behaviors.content_lock = {
+  attach: function(context) {
   window.content_lock_onleave = function  () {
     var nid = Drupal.settings.content_lock.nid;
     var ajax_key = Drupal.settings.content_lock.ajax_key;
-    $.ajax({
+    jQuery.ajax({
       url: Drupal.settings.basePath + 'ajax/content_lock/'+nid+'/canceledit',
       data: {k: ajax_key},
       async: false,
@@ -19,11 +20,10 @@ Drupal.behaviors.content_lock = function() {
     return Drupal.t(Drupal.settings.content_lock.unload_js_message);
   }
 
-  $(document).ready(function() {
-    $().onUserExit( {
+    jQuery().onUserExit( {
       execute: content_lock_onleave,
       executeConfirm: content_lock_confirm,
       internalURLs: 'canceledit|trash/confirm|edit'
     });
-  });
+  }
 };
